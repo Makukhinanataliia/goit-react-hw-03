76ed41baa37f51f50105287bb6c903f48ca4f0fc
Makukhinanataliia <makukhinanataliia@gmail.com>
2025-04-29 18:27:35 +0330

    finish

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,22 @@
+import { FaPhone } from "react-icons/fa";
 import style from "./Contact.module.css";
 
 export default function Contact({ contact, onDelete }) {
+  const avatarUrl = `https://ui-avatars.com/api/?name=${encodeURIComponent(
+    contact.name
+  )}&background=random&size=32`;
+
   return (
     <li className={style.item}>
-      {contact.name}: {contact.number}
+      <div className={style.contactInfo}>
+        <img src={avatarUrl} alt={contact.name} className={style.avatar} />
+        <div className={style.details}>
+          <span className={style.name}>{contact.name}</span>
+          <span className={style.number}>
+            <FaPhone className={style.phoneIcon} /> {contact.number}
+          </span>
+        </div>
+      </div>
       <button
         type="button"
         className={style.button}
